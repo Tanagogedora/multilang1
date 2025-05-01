42fa02e4288f5057a7ae53c76e57336fc530eecd
Tanagotti <tanagogedora@gmail.com>
2025-05-01 11:38:02 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,25 @@
-namespace maltilang
-{
+namespace maltilang {
     // ブロック内選択リスト
-    export enum SelcNo1
-    {
-        //% blockId="itemlight"
-        //% block="@Localization(local.ja-JP) 光"
-        //% block="@Localization(local.en) Light"
-            Light,
-        //% blockId="itemstar"   
-        //% block="@Localozation(local.ja-JP) 星"
-        //% block="@Localization(local.en) Star"
-            Star,
-        //% blockId="itemmoon"
-        //% block="@Localization(local.ja-JP) 月"
-        //% block="@Localization(local.en) Moon"
-            Moon
+    export enum SelcNo1 {
+        //% block="@localization(locale.ja-JP) 光"
+        //% block="@localization(locale.en) Light"
+        Light,
+
+        //% block="@localization(locale.ja-JP) 星"
+        //% block="@localization(locale.en) Star"
+        Star,
+
+        //% block="@localization(locale.ja-JP) 月"
+        //% block="@localization(locale.en) Moon"
+        Moon
     }
 
-    // 拡張ブロック
+    // ブロック本体
     //% blockId="SelectItems"
-    //% block="@Localization(local.ja-JP) アイテム %item"
-    //% block="@Localization(local.en) Item %item"
-    // weight=80, blockGap=8
-    export function Select_Items(item:SelcNo1):string
-    {
+    //% block="@localization(locale.ja-JP) アイテム %item"
+    //% block="@localization(locale.en) Item %item"
+    //% weight=80 blockGap=8
+    export function Select_Items(item: SelcNo1): string {
         return item.toString();
-        
-    }       
+    }
 }
